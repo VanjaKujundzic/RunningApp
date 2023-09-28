136ac1adcb2cc945d837c3bb07150ecaf51f381a
VanjaKujundzic <Kujundzicv97@gmail.com>
2023-09-28 18:00:33 +0200

    Add a function to edit text and photos

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -4,6 +4,7 @@ using RunGroupWebApp.Data;
 using RunGroupWebApp.Interfaces;
 using RunGroupWebApp.Models;
 using RunGroupWebApp.ViewModels;
+using System.Diagnostics.Eventing.Reader;
 
 namespace RunGroupWebApp.Controllers
 {
@@ -68,6 +69,64 @@ namespace RunGroupWebApp.Controllers
 
            
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var club = await _clubRepository.GetByIdAsync(id);
+            if (club == null) return View("Error");
+            var clubVm = new EditClubViewModel
+            {
+                Title = club.Title,
+                Description = club.Description,
+                AddressId = (int)club.AddressId,
+                Address = club.Address,
+                URL = club.Image,
+                ClubCategory = club.ClubCategory
+            };
+            return View(clubVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditClubViewModel clubVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit club");
+                return View("Edit", clubVm);
+            }
+            var userClub = await _clubRepository.GetByIdAsyncNoTracking(id);
+
+            if (userClub != null)
+            {
+                try
+                {
+                    await _photoService.DelePhotoAsync(userClub.Image);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Could not delete photo");
+                    return View(clubVm);
+                }
+                var photoResult = await _photoService.AddPhotoAsync(clubVm.Image);
+
+                var club = new Club
+                {
+                    Id = id,
+                    Title = clubVm.Title,
+                    Description = clubVm.Description,
+                    Image = photoResult.Url.ToString(),
+                    AddressId = clubVm.AddressId,
+                    Address = clubVm.Address,
+                };
+                _clubRepository.Update(club);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(clubVm);
+            }  
+        }
+
     }
 
     
